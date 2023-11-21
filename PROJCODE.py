6db36f911ab18f2dd1d1a7a044adb7716656115a
aahmedfahmii <ae2200926@tkh.edu.eg>
2023-11-22 00:21:52 +0200

    modified logical error in decryption and encryption that had to do with spaces. modified decryption where i asked it to append from my new dictionary(new_dict) not the initial one(my_dictionary) did a user friendly application that runs my code asks for a users name then asks them to decrypt or encrypt then gives them translations and original.

--- PROJCODE.py
+++ PROJCODE.py
@@ -1,5 +1,5 @@
 #FIRST AND FOREMOST I AM CREATING A DICTIONARY THAT BASICALLY MAPS CHARACTERS (COULD BE LETTER,NUMBERS OR EVEN PUNCTUATION MARKS) TO THEIR INTERRELATED MORSE CODE REPRESENTATIONS.
-#CURLY BRACES USED TO INITIALIZE AND DEFINE MY DICTIONARY  
+#CURLY BRACES USED TO INITIALIZE AND DEFINE MY DICTIONARY 
 my_dictionary = {                                        
 'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -27,6 +27,9 @@ def encryption(word):
             morse_bank.append(my_dictionary[letter])     #if letter is in the morse dictionary it will add its morse representation to the empty list
             index += 1
 
+        elif letter == ' ':
+            morse_bank.append('|')     #if a space is given it will return '|'
+             
         else:   
             morse_bank.append("#")     #application will return "#" for unknown characters
             index += 1
@@ -47,12 +50,49 @@ def decryption(code):
     for i in code:
         
         if i in new_dict:
-            letter_bank.append(my_dictionary[i])
+            letter_bank.append(new_dict[i])
 
-        elif i == ' ':
-            letter_bank.append('|')       #if a space is given it will return '|' 
+        elif i == '|':
+            letter_bank.append(' ')      
 
         else:   
             letter_bank.append("#")     #application will return "#" for unknown characters
     
     return ''.join(letter_bank)    #join method used to bring the elements in the letter_bank list together without anything between them which is represented by the empty string ('')
+
+#DEFINING A FUNCTION TO RUN MY CODE, THIS FUNCTION COMPLETES MY APPLICATION WHERE IT ALLOWS THE USER TO CHOOSE OUT OF THREE OPTIONS (E,D,X)
+#THE USER THEN INPUTS EITHER LETTERS OR MORSE CODE BASED ON HIS/HER CHOICE
+#THE APPLICATION THEN TRANSLATES THE USER'S INPUT USING THE ENCRYPTION AND DECRYPTION FUNCTIONS
+
+def start():
+
+    name = input("PLEASE ENTER YOUR NAME: ")
+
+    while True:                     #start an infinite loop to allow user to use the application multiple times until he/she decides to quit
+
+        programme = input(
+    '''CHOOSE PROGRAMME:
+'E' : ENCRYPTION
+'D' : DECRYPTION
+'X' : EXIT
+'''
+).upper()               #changes any user input to capital letters to prevent from handling errors, ensures consistency and a more user friendly experience where it forgives regardless of capital or lower case input
+        
+        if programme == 'E':
+            start_e = input("WELCOME TO THE ENCRYPTION REALM, PLEASE ENTER YOUR LETTERS: ")
+            print('ORIGINAL TEXT:' , start_e)
+            print('MORSE CODE:' , encryption(start_e))
+        elif programme == 'D':
+            print("PLEASE ENTER EACH LETTER'S MORSE CODE REPRESENTATION WITH EXACTLY 1 SPACE IN BETWEEN. (e.g. ABC = .- -... -.-. )")
+            start_d = input("WELCOME TO THE DECRYPTION REALM, PLEASE ENTER YOUR MORSE CODE: ")
+            print('ORIGINAL CODE:' , start_d)
+            print('DECRYPTED TEXT:' , decryption(start_d))
+
+        elif programme == 'X':
+            print(f'THANK YOU {name.upper()} YOU HAVE SUCCESSFULLY ENDED YOUR SESSION')
+            break
+
+        else:
+            print("UNKOWN PROGRAMME, PLEASE CHOOSE 'E FOR ENCRYPTION', 'D FOR DECRYPTION' OR 'X' TO EXIT")
+
+    start()
