66ec87fa577fcc1b62e18a50d1f16d15000e9bf6
aahmedfahmii <ae2200926@tkh.edu.eg>
2023-11-21 14:45:31 +0200

    added encryption function and corrected the comment about encryption

--- PROJCODE.py
+++ PROJCODE.py
@@ -10,3 +10,20 @@ my_dictionary = {
 '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
 '0': '-----', ' ': '|', ',': '--..--', '.': '.-.-.-', '?': '..--..', '!': '-.-.--'
 }
+
+#DEFINING MY ENCRYPTION FUNCTION WHICH IS GOING TO TRANSLATE CODE FROM NORMAL ENGLISH LETTERS TO MORSE CODE
+
+def encryption(word):
+
+    morse_bank = []  #empty empty list to store user input
+    
+    for i in word.upper():      # used upper method to change all given characters to uppercase
+                                #for loop iterates over all given letters
+        if i in my_dictionary:     
+            morse_bank.append(my_dictionary[i])     #if letter is in the morse dictionary it will add its morse representation to the empty list
+
+        else:   
+            morse_bank.append(" ")     #if the letter is not in the morse dictionary it will return nothing
+    
+    return (morse_bank)
+
