ce72e659c3937612e3ea6fe20682171250e33253
aahmedfahmii <ae2200926@tkh.edu.eg>
2023-11-22 16:09:47 +0200

    fixed unittesting to match the adjustment made to projcode which was a space added after each letter in encryption

--- UnitTesting.py
+++ UnitTesting.py
@@ -4,8 +4,8 @@ import PROJCODE
 class TestMorseCodeConversion(unittest.TestCase):
 
     def test_encryption(self):
-        self.assertEqual(PROJCODE.encryption("AHMED"), ".-....--.-..")
-        self.assertEqual(PROJCODE.encryption("FAHMI 2005"), "..-..-....--..|..-------------.....")
+        self.assertEqual(PROJCODE.encryption("AHMED"), ".- .... -- . -.. ")
+        self.assertEqual(PROJCODE.encryption("FAHMI 2005"), "..-. .- .... -- .. | ..--- ----- ----- ..... ")
 
     def test_decryption(self):
         self.assertEqual(PROJCODE.decryption(".- -... -.-."), "ABC")
