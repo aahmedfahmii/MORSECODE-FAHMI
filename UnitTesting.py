2fe320f1381d0f6a487ae0f05429e9ea186a41b1
aahmedfahmii <ae2200926@tkh.edu.eg>
2023-11-22 18:46:31 +0200

    finished all encryption cases

--- UnitTesting.py
+++ UnitTesting.py
@@ -1,16 +1,37 @@
 import unittest
 import PROJCODE
 
-class TestMorseCodeConversion(unittest.TestCase):
+class TestMorseTranslation(unittest.TestCase):
 
-    def test_encryption(self):
-        self.assertEqual(PROJCODE.encryption("AHMED"), ".- .... -- . -.. ")
-        self.assertEqual(PROJCODE.encryption("FAHMI 2005"), "..-. .- .... -- .. | ..--- ----- ----- ..... ")
+#ENCRYPTION CASES 
 
-    def test_decryption(self):
-        self.assertEqual(PROJCODE.decryption(".- -... -.-."), "ABC")
-        self.assertEqual(PROJCODE.decryption(".- -... -.-. | ..--- ----- ----- ....."), "ABC 2005")
+    def test_encryption(characters):
+        characters.assertEqual(PROJCODE.encryption("AHMED"), ".- .... -- . -.. ")
+        characters.assertEqual(PROJCODE.encryption("FAHMI 2005"), "..-. .- .... -- .. | ..--- ----- ----- ..... ")
 
+    def test_incorrect_user_input_e(characters):
+        characters.assertEqual(PROJCODE.encryption("$%^&*@"), "######")
+
+    def test_special_characters(characters):
+        characters.assertEqual(PROJCODE.encryption("7 PERCENT %"), "--... | .--. . .-. -.-. . -. - | #")
+        characters.assertEqual(PROJCODE.encryption("1 dollar $"), ".---- | -.. --- .-.. .-.. .- .-. | #")
+
+    def test_case_sensitivity(characters):
+        characters.assertEqual(PROJCODE.encryption("uppercase"), "..- .--. .--. . .-. -.-. .- ... . ")
+        characters.assertEqual(PROJCODE.encryption("UppErCaSe"), "..- .--. .--. . .-. -.-. .- ... . ")
+        
+    def test_empty_string(characters):
+        characters.assertEqual(PROJCODE.encryption(""), "")
+
+    def test_phrase_inc_punctuation_e(characters):
+        characters.assertEqual(PROJCODE.encryption("WHO, IS. THIS! MAN?"), ".-- .... --- --..-- | .. ... .-.-.- | - .... .. ... -.-.-- | -- .- -. ..--.. ")
+
+
+#DECRYPTION CASES
+     
+    def test_decryption(characters):
+        characters.assertEqual(PROJCODE.decryption(".- -... -.-."), "ABC")
+        characters.assertEqual(PROJCODE.decryption(".- -... -.-. | ..--- ----- ----- ....."), "ABC 2005")
 
 if __name__ == '__main__':
     unittest.main()
